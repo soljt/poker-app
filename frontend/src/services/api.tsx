1ad078cec2431dcd17e0f863a33671ce9f2087c9
Sol Thiessen <soljthiessen@gmail.com>
2025-07-08 15:27:29 +0200

    solved stale csrf token issue (refreshed token wasn't being added to header)

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(";").shift();
+}
+
 export const api = axios.create({
   baseURL: "http://localhost:5000",
 });
@@ -17,4 +23,15 @@ export const game_api = axios.create({
 export const admin_api = axios.create({
   baseURL: "http://localhost:5000/admin",
   withCredentials: true,
+});
+
+// Set interceptors to always inject fresh CSRF token
+[api, auth_api, game_api, admin_api].forEach((api) => {
+  api.interceptors.request.use((config) => {
+    const token = getCookie("csrf_access_token");
+    if (token) {
+      config.headers["X-CSRF-TOKEN"] = token;
+    }
+    return config;
+  });
 });
--- frontend/src/views/Admin.tsx
+++ frontend/src/views/Admin.tsx
@@ -3,42 +3,13 @@ import UserRow from "../components/UserRow";
 import UserForm from "../components/UserForm";
 import Pagination from "../components/Pagination";
 import { UserRowData } from "../types";
-import { admin_api, auth_api, game_api } from "../services/api";
+import { admin_api } from "../services/api";
 import { Container } from "react-bootstrap";
 
 export default function Admin() {
   const [users, setUsers] = useState<UserRowData[]>([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-
-  function getCookie(name: string) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop()?.split(";").shift();
-  }
-
-  useEffect(() => {
-    const refreshCSRF = async () => {
-      try {
-        // Make a POST to who_am_i to refresh token & get a fresh CSRF cookie
-        await auth_api.post("/who_am_i", {});
-
-        // Read the new CSRF cookie
-        const csrf = getCookie("csrf_access_token");
-
-        // Update axios instances
-        if (csrf !== null) {
-          auth_api.defaults.headers.common["X-CSRF-TOKEN"] = csrf;
-          game_api.defaults.headers.common["X-CSRF-TOKEN"] = csrf;
-          admin_api.defaults.headers.common["X-CSRF-TOKEN"] = csrf;
-        }
-      } catch (err) {
-        console.error("Failed to refresh CSRF token:", err);
-      }
-    };
-
-    refreshCSRF();
-  }, []);
 
   const fetchUsers = useCallback(() => {
     admin_api.get(`users?page=${page}`).then((res) => {
