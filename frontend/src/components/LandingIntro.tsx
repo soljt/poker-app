5cbc80e61a9b1ee9938656088adc2c235c5da37c
Sol Thiessen <soljthiessen@gmail.com>
2025-07-12 18:29:26 +0200

    typo fix

--- frontend/src/components/LandingIntro.tsx
+++ frontend/src/components/LandingIntro.tsx
@@ -6,7 +6,7 @@ const LandingIntro = () => {
     <Container>
       <h1 className="display-1">Welcome to poker.soljt.ch!</h1>
       <p className="lead fs-4">
-        If you already know how to player poker, you can see who's playing and
+        If you already know how to play poker, you can see who's playing and
         join a table right away by clicking the button below:
       </p>
 
