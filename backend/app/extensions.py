393cec2e60c5df617ce5b89b8580847d631639f3
Sol Thiessen <soljthiessen@gmail.com>
2026-03-24 23:54:58 +0100

    adjusted to manage dev vs prod

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -12,5 +12,13 @@ else:
     socketio = SocketIO(cors_allowed_origins="*")
 
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")
-limiter = Limiter(key_func=get_remote_address, storage_uri=f"redis://:{REDIS_PASSWORD}@{os.getenv('CACHE_REDIS_HOST', 'localhost')}:{os.getenv('CACHE_REDIS_PORT', '6379')}")
+REDIS_HOST = os.getenv("CACHE_REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("CACHE_REDIS_PORT", "6379")
+
+if os.getenv("FLASK_ENV") == "production":
+    redis_uri = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}"
+    limiter = Limiter(key_func=get_remote_address, storage_uri=redis_uri)
+else:
+    limiter = Limiter(key_func=get_remote_address)  # in-memory for dev
+
 cache = Cache()
